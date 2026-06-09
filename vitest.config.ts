3d769261e33b650c2248b3d27a25302908ad3170
Tim Pearson <gouki9@gmail.com>
2026-06-09 15:51:49 -0400

    Mock Electron in Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src/renderer'),
-      '@shared': resolve(__dirname, 'src/shared')
+      '@shared': resolve(__dirname, 'src/shared'),
+      electron: resolve(__dirname, 'src/test/mocks/electron.ts')
     }
   },
   test: {
